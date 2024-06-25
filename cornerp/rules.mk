b51afc7ab33859d557e8c47e3de732317581b191
vim-workcomputer <lincoo2436@outlook.com>
2024-06-25 10:52:21 +0800

    Update rules.mk

--- cornerp/rules.mk
+++ cornerp/rules.mk
@@ -5,13 +5,13 @@ BOOTLOADER = rp2040
 #   change yes to no to disable
 #
 BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
-MOUSEKEY_ENABLE = no       # Mouse keys
+MOUSEKEY_ENABLE = yes       # Mouse keys
 EXTRAKEY_ENABLE = yes       # Audio control and System control
 CONSOLE_ENABLE = no         # Console for debug
 COMMAND_ENABLE = no         # Commands for debug and configuration
 NKRO_ENABLE = no            # Enable N-Key Rollover
-BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
-RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
+BACKLIGHT_ENABLE = yes       # Enable keyboard backlight functionality
+RGBLIGHT_ENABLE = yes        # Enable keyboard RGB underglow
 UNICODE_ENABLE = yes        # Unicode
 AUDIO_ENABLE = no           # Audio output
 SPLIT_KEYBOARD = yes        # Use shared split_common code
@@ -21,3 +21,4 @@ OLED_DRIVER     = ssd1306
 LTO_ENABLE      = yes
 TRI_LAYER_ENABLE = yes
 TAP_DANCE_ENABLE = yes
+VIA_ENABLE         = yes
